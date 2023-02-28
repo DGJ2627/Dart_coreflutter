75456614927faf9aaafbae4c4d2147e0c9407dd5
DGJ2627 <gautamjethava160@gmail.com>
2023-02-28 19:05:09 +0530

    UNIT-8

---
+++ lec1/1/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:u8_1/views/screens/Firstpage.dart';
+
+void main () {
+  runApp(
+    Gautam(),
+  );
+
+}
+
+class Gautam extends StatelessWidget {
+  const Gautam({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Firstpage()
+    );
+  }
+}
---
+++ lec2/1/android/app/src/main/kotlin/com/example/u8_2_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.u8_2_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
