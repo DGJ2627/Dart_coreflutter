c27967dfc43f07f099eb51e936a4fc0befee6bb4
Gautam <114165275+DGJ2627@users.noreply.github.com>
2023-04-28 21:54:38 +0530

    Update stop_watch.dart

--- lib/screens/views/stop_watch.dart
+++ lib/screens/views/stop_watch.dart
@@ -2,76 +2,99 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-class Gautam extends StatefulWidget {
-  const Gautam({Key? key}) : super(key: key);
+class stop_page extends StatefulWidget {
+  const stop_page({Key? key}) : super(key: key);
 
   @override
-  State<Gautam> createState() => _GautamState();
+  State<stop_page> createState() => _stop_pageState();
 }
 
-class _GautamState extends State<Gautam> {
-  int _elapsedTime = 0;
-  late Timer _timer;
+class _stop_pageState extends State<stop_page> {
+  int sec = 0;
+  int min = 0;
+  int hw = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Stop Watch"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text("Elapsed Time: $_elapsedTime"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  child: Text("Start"),
-                  onPressed: _startTimer,
-                ),
-                SizedBox(width: 8),
-                ElevatedButton(
-                  child: Text("Stop"),
-                  onPressed: _stopTimer,
-                ),
-                SizedBox(width: 8),
-                ElevatedButton(
-                  child: Text("Reset"),
-                  onPressed: _resetTimer,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
-  void _startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        _elapsedTime++;
+
+  Time() {
+    setState(() {
+      Future.delayed(const Duration(seconds: 1), () {
+        sec++;
+
+        if (hw > 12) {
+          hw -= 12;
+        }
+        if (sec > 59) {
+          min++;
+          sec = 0;
+        }
+        if (min > 59) {
+          hw++;
+          min = 0;
+        }
+        Time();
       });
     });
   }
-
-  void _stopTimer() {
-    _timer.cancel();
-  }
-
-  void _resetTimer() {
-    _timer.cancel();
-    setState(() {
-      _elapsedTime = 0;
-    });
-  }
-
   @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
+  Widget build(BuildContext context) {
+    Size s = MediaQuery.of(context).size;
+    double w = s.width;
+    double h = s.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Timer"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: h * 0.5,
+            width: w,
+            alignment: Alignment.center,
+            child: Text(
+              "$hw : $min : $sec",
+              style: TextStyle(
+                fontSize: 50,
+              ),
+            ),
+          ),
+          Container(
+            height: h * 0.2,
+            width: w,
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Time();
+                  },
+                  icon: Icon(Icons.play_arrow),
+                ),
+                IconButton(
+                  onPressed: () {
+                    sec = sec;
+                  },
+                  icon: Icon(Icons.stop),
+                ),
+                IconButton(
+                  onPressed: () async{
+                    sec = 1;
+                    min = 0;
+                    hw = 0;
+
+                    await Time();
+                    sec --;
+                  },
+                  icon: Icon(Icons.restart_alt),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
 
